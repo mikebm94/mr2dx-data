8f723b4b3504107b3b81c7a00134d50df356f7e5
mikebm94 <mbmiller1994@outlook.com>
2022-04-02 17:32:31 -0500

    Map friendly names to game file paths

--- tools/common.ps1
+++ tools/common.ps1
@@ -22,6 +22,64 @@ $FinishedDataPath = Resolve-Path (Join-Path $DataPath 'csv/')
 $ImageDataPath = Resolve-Path (Join-Path $DataPath 'images/')
 
 
+# Maps a friendly name to a game file path.
+$GameFiles = @{
+    # CSV table of all monster variations that can be obtained at the shrine.
+    'ShrineMonsters'     = 'SDATA_MONSTER.csv'
+
+    # Maps each song in the English music database to a corresponding
+    # shrine monster ID. Also maps to an ID of an offset that is applied
+    # to the monster after shrining.
+    'ShrineMonsterSongs' = 'en_sqlout_b.txt'
+
+    # Defines all songs in the English music database.
+    'ShrineSongs'        = 'en_sqlout_name.txt'
+
+    # Defines techniques available to a breed.
+    'Techs_ka'           = 'mf2/data/mon/kapi/ka_ka_wz.dat'  # Pixie
+    'Techs_kb'           = 'mf2/data/mon/kbdr/kb_kb_wz.dat'  # Dragon
+    'Techs_kc'           = 'mf2/data/mon/kckn/kc_kc_wz.dat'  # Centaur
+    'Techs_kd'           = 'mf2/data/mon/kdro/kd_kd_wz.dat'  # ColorPandora
+    'Techs_ke'           = 'mf2/data/mon/kebe/ke_ke_wz.dat'  # Beaclon
+    'Techs_kf'           = 'mf2/data/mon/kfhe/kf_kf_wz.dat'  # Henger
+    'Techs_kh'           = 'mf2/data/mon/khcy/kh_kh_wz.dat'  # Wracky
+    'Techs_ki'           = 'mf2/data/mon/kigo/ki_ki_wz.dat'  # Golem
+    'Techs_kk'           = 'mf2/data/mon/kkro/kk_kk_wz.dat'  # Zuum
+    'Techs_kl'           = 'mf2/data/mon/klyo/kl_kl_wz.dat'  # Durahan
+    'Techs_km'           = 'mf2/data/mon/kmto/km_km_wz.dat'  # Arrow Head
+    'Techs_ma'           = 'mf2/data/mon/marig/ma_ma_wz.dat' # Tiger
+    'Techs_mb'           = 'mf2/data/mon/mbhop/mb_mb_wz.dat' # Hopper
+    'Techs_mc'           = 'mf2/data/mon/mcham/mc_mc_wz.dat' # Hare
+    'Techs_md'           = 'mf2/data/mon/mdbak/md_md_wz.dat' # Baku
+    'Techs_me'           = 'mf2/data/mon/megar/me_me_wz.dat' # Gali
+    'Techs_mf'           = 'mf2/data/mon/mfakr/mf_mf_wz.dat' # Kato
+    'Techs_mg'           = 'mf2/data/mon/mggjr/mg_mg_wz.dat' # Zilla
+    'Techs_mh'           = 'mf2/data/mon/mhlam/mh_mh_wz.dat' # Bajarl
+    'Techs_mi'           = 'mf2/data/mon/minya/mi_mi_wz.dat' # Mew
+    'Techs_mj'           = 'mf2/data/mon/mjfbd/mj_mj_wz.dat' # Phoenix
+    'Techs_mk'           = 'mf2/data/mon/mkgho/mk_mk_wz.dat' # Ghost
+    'Techs_ml'           = 'mf2/data/mon/mlspm/ml_ml_wz.dat' # Metalner
+    'Techs_mm'           = 'mf2/data/mon/mmxsu/mm_mm_wz.dat' # Suezo
+    'Techs_mn'           = 'mf2/data/mon/mnsnm/mn_mn_wz.dat' # Jill
+    'Techs_mo'           = 'mf2/data/mon/mochy/mo_mo_wz.dat' # Mochi
+    'Techs_mp'           = 'mf2/data/mon/mpjok/mp_mp_wz.dat' # Joker
+    'Techs_mq'           = 'mf2/data/mon/mqnen/mq_mq_wz.dat' # Gaboo
+    'Techs_mr'           = 'mf2/data/mon/mrpru/mr_mr_wz.dat' # Jell
+    'Techs_ms'           = 'mf2/data/mon/msund/ms_ms_wz.dat' # Undine
+    'Techs_mt'           = 'mf2/data/mon/mtgai/mt_mt_wz.dat' # Niton
+    'Techs_mu'           = 'mf2/data/mon/muoku/mu_mu_wz.dat' # Mock
+    'Techs_mv'           = 'mf2/data/mon/mvdak/mv_mv_wz.dat' # Ducken
+    'Techs_mw'           = 'mf2/data/mon/mwpla/mw_mw_wz.dat' # Plant
+    'Techs_mx'           = 'mf2/data/mon/mxris/mx_mx_wz.dat' # Monol
+    'Techs_my'           = 'mf2/data/mon/mylau/my_my_wz.dat' # Ape
+    'Techs_mz'           = 'mf2/data/mon/mzmus/mz_mz_wz.dat' # Worm
+    'Techs_na'           = 'mf2/data/mon/naaga/na_na_wz.dat' # Naga
+
+    # Defines baseline stats and parameters for all breeds.
+    'MonsterBaselines'   = 'mf2/data/monbase/mon_base.dat'
+}
+
+
 <#
 .SYNOPSIS
 Create a hashtable from a collection of objects
