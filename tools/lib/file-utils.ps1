bc706006c6db5c74da26515d20bcfe4941c05f04
mikebm94 <mbmiller1994@outlook.com>
2022-04-14 20:59:26 -0500

    Fix sourcing incorrect script path

--- tools/lib/file-utils.ps1
+++ tools/lib/file-utils.ps1
@@ -5,7 +5,7 @@
         without the need to change multiple files.
 #>
 
-. (Join-Path $PSScriptRoot 'file-manifiests.ps1')
+. (Join-Path $PSScriptRoot 'file-manifests.ps1')
 . (Join-Path $PSScriptRoot 'misc-utils.ps1')
 
 
