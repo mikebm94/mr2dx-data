39ddb02ddbd671c3ff766bf2338e32d7a058a83f
mikebm94 <mbmiller1994@outlook.com>
2022-04-23 08:07:37 -0500

    Add ability to specify which directories to clean

--- tools/clean.ps1
+++ tools/clean.ps1
@@ -6,22 +6,33 @@ Cleans all generated data files.
 #>
 
 [CmdletBinding()]
-param()
+param(
+    # The names of the file manifests
+    # for which their directory should be cleaned.
+    [Parameter(Position = 0, ValueFromRemainingArguments)]
+    [ValidateNotNull()]
+    [string[]]
+    $CleanTargets = @(
+        'FinishedData', 'ExtractedData', 'ScrapedData', 'GameFiles'
+    )
+)
 
 
 $ErrorActionPreference = 'Stop'
 
-. (Join-Path $PSScriptRoot 'lib/paths.ps1')
+. (Join-Path $PSScriptRoot 'lib/file-manifests.ps1')
 
 
-$DirectoriesToClean = @(
-    $FinishedDataPath,
-    $ExtractedIntermediateDataPath,
-    $ScrapedIntermediateDataPath,
-    $GameFilesPath
-)
+foreach ($manifestName in $CleanTargets) {
+    $manifest = $FileManifests[$manifestName]
 
+    if ($null -eq $manifest) {
+        throw "Cannot clean file manifest '${manifestName}': " +
+        "Manifest does not exist."
+    }
 
-foreach ($path in $DirectoriesToClean) {
-    Remove-Item -ErrorAction Ignore -Recurse -Force -Path (Join-Path $path '*')
+    Write-Host "Cleaning directory '$( $manifest.Directory )' ..."
+
+    $path = (Join-Path $manifest.Directory '*')
+    Remove-Item -ErrorAction Ignore -Recurse -Force -Path $path
 }
