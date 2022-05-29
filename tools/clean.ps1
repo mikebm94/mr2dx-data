f00fe51449b85ac28555fe8880520635e2008af1
mikebm94 <mbmiller1994@outlook.com>
2022-05-29 17:49:19 -0500

    Only clean files defined in the manifests

--- tools/clean.ps1
+++ tools/clean.ps1
@@ -7,33 +7,22 @@ Cleans all generated data files.
 
 [CmdletBinding()]
 param(
-    # The names of the file manifests
-    # for which their directory should be cleaned.
+    # The names of the file manifests for which their directory should be cleaned.
     [Parameter(Position = 0, ValueFromRemainingArguments)]
     [ValidateNotNull()]
     [ValidateSet('FinishedData', 'ExtractedData', 'ScrapedData', 'GameFiles')]
     [string[]]
-    $CleanTargets = @(
-        'FinishedData', 'ExtractedData', 'ScrapedData', 'GameFiles'
-    )
+    $CleanTargets = @( 'FinishedData', 'ExtractedData', 'ScrapedData', 'GameFiles' )
 )
 
 
 $ErrorActionPreference = 'Stop'
 
-. (Join-Path $PSScriptRoot 'lib/file-manifests.ps1')
+. (Join-Path $PSScriptRoot 'lib/file-utils.ps1')
 
 
-foreach ($manifestName in $CleanTargets) {
-    $manifest = $FileManifests[$manifestName]
-
-    if ($null -eq $manifest) {
-        throw "Cannot clean file manifest '${manifestName}': " +
-        "Manifest does not exist."
+$CleanTargets | Get-ManifestFileInfo | ForEach-Object {
+    if (Test-Path -LiteralPath $PSItem.FullPath) {
+        Remove-Item -LiteralPath $PSItem.FullPath -Force -ErrorAction Continue
     }
-
-    Write-Host "Cleaning directory '$( $manifest.Directory )' ..."
-
-    $path = (Join-Path $manifest.Directory '*')
-    Remove-Item -ErrorAction Ignore -Recurse -Force -Path $path
 }
