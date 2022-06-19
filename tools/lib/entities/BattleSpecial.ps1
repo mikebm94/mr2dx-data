c61ea98f72d68c910c6cbfa1a070ce23cbaabe67
mikebm94 <mbmiller1994@outlook.com>
2022-06-18 20:51:34 -0500

    Add 'Drunk' battle special

--- tools/lib/entities/BattleSpecial.ps1
+++ tools/lib/entities/BattleSpecial.ps1
@@ -3,8 +3,7 @@
     can possess and activate during battle.
 #>
 class BattleSpecial {
-    # 12 is missing because that battle special ("Drunk") is not used in the game.
-    [ValidateSet(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13)]
+    [ValidateRange(1, 13)]
     [int] $BattleSpecialId
 
     [ValidateLength(3, 12)]
@@ -14,9 +13,7 @@ class BattleSpecial {
     # The priority of the battle special over other battle specials.
     # If the battle special attempts to trigger while another battle special is active,
     # it will not trigger if the other battle special has a higher priority (lower number.)
-    #
-    # Priority 6 is missing because that battle speciaL ("Drunk") is not used in the game.
-    [ValidateSet(0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12)]
+    [ValidateRange(0, 12)]
     [int] $TriggerPriority
 
     # The analysis that Dadge will give when a monster possesses this battle special.
@@ -35,7 +32,7 @@ class BattleSpecialIntermediate : BattleSpecial {
     # Flag name used in the games data files to specify a battle special.
     [ValidateSet(
         'SOKO', 'GYAKU', 'KONJO', 'SHUCHU', 'TOUKON', 'FUNNU',
-        'GAMAN', 'YOYU', 'HISSHI', 'GENKI', 'HONKI', 'DANKETSU'
+        'GAMAN', 'YOYU', 'HISSHI', 'GENKI', 'HONKI', 'DEISUI', 'DANKETSU'
     )]
     [string] $Flag
 }
